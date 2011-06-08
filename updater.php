39259aa4bee3498a3e50ad14fb8dd711a282a5ce
Douglas Beck <dbeck@douglasbeck.com>
2011-06-08 18:26:08 -0400

    updating url

--- updater.php
+++ updater.php
@@ -1,11 +1,11 @@
 <?php
 /*
 Plugin Name: Theme Updater
-Plugin URI: http://webcom.dev.smca.ucf.edu/
+Plugin URI: https://github.com/UCF/Theme-Updater
 Description: A theme updater for GitHub hosted Wordpress themes.  This Wordpress plugin automatically checks GitHub for theme updates and enables automatic install.  For more information read <a href="https://github.com/UCF/Theme-Updater/blob/master/readme.markdown">plugin documentation</a>.
 Author: Douglas Beck
 Author: UCF Web Communications
-Version: 1.3.1
+Version: 1.3.2
 */
 
 require_once('assets.php');
